b74b3f5734c8a96c8126949ebc1d827279e17809
mariaecandido6-lgtm <maria.e.candido6@edu.senai.br>
2026-06-11 11:51:11 -0300

    mensagem do commit

---
+++ atividade1.js
@@ -0,0 +1,15 @@
+// Nível 1- ex.1: Lista de nomes
+
+let alunos = ["Pamela", "Paloma", "Gabriela", "Leticia", "Emanuelly"];
+console.log(alunos)
+console.log([alunos[0], alunos[4]]);
+
+console.table(alunos)
+
+
+//ex2:  Lista de frutas
+let frutas = ["Banana", "Morango", "Manga", "Uva"];
+console.log("Banana");
+console.log("Morango");
+console.log("Manga");
+console.log("Uva")
